10789f5e20f7f9c15ddbca05e1f251921acba44b
oma <oluomaihejiofor@gmail.com>
2025-02-06 08:11:28 +0100

    feat: added and configured chakraUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
 )
